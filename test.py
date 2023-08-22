7c589c126d3c800cd2fff58ada89d783350b98a6
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2023-08-22 12:01:04 +0300

    Update test.py

--- test.py
+++ test.py
@@ -72,13 +72,13 @@ def main():
     q12_choices = ["Inconsistent with the customer's profile ", "Unusual or unexplained", "Consistent with the customer's financial behavior ", "Lacks an apparent economic purpose "]   
     q12_answer = st.radio("Question 12: Which of the following is NOT a characteristic of a suspicious transaction?", q12_choices)
 
-    q13_choices = [ “A pat on the back”, “A written warning”, “Fine and imprisonment”, “No penalty, it's encouraged”]
+    q13_choices = [“A pat on the back”, “A written warning”, “Fine and imprisonment”, “No penalty, it's encouraged”]
     q13_answer = st.radio("Question 11: What is the penalty for tipping off, where someone discloses that a suspicious transaction has been reported? ", q13_choices)
 
-    q14choices = [ “Marketing the institution's services “, ” Ensuring that the institution doesn't make a profit”, “Overseeing and implementing AML policies and procedures”, “Choosing the office's color scheme”]   
+    q14choices = [“Marketing the institution's services “, ” Ensuring that the institution doesn't make a profit”, “Overseeing and implementing AML policies and procedures”, “Choosing the office's color scheme”]   
     q14_answer = st.radio("Question 14: " What is the role of the AML Compliance Officer within a financial institution?”, q14_choices)
     
-    q15_choices = [" Kenyan Police Department”, “Kenya Revenue Authority (KRA)", "Insurance Regulatory Authority (IRA)”, “Central Bank of Kenya (CBK)"]   
+    q15_choices = ["Kenyan Police Department”, “Kenya Revenue Authority (KRA)", "Insurance Regulatory Authority (IRA)”, “Central Bank of Kenya (CBK)"]   
     q15_answer = st.radio("Question 15:  Which regulatory body in Kenya oversees and enforces AML regulations for insurance companies?", q15_choices)
 
 
